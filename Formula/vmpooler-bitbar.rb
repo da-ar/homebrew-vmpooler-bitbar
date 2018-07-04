a8a5c84f21f1a2b0501119ef3bb46be1a99900df
John McCabe <john@johnmccabe.net>
2018-07-04 01:36:35 +0100

    v2.1.0 release

--- Formula/vmpooler-bitbar.rb
+++ Formula/vmpooler-bitbar.rb
@@ -1,10 +1,10 @@
 class VmpoolerBitbar < Formula
     desc "Manage vmpooler from your OSX menubar"
     homepage "https://github.com/johnmccabe/vmpooler-bitbar/"
-    url "https://github.com/johnmccabe/vmpooler-bitbar/releases/download/v2.0.0/vmpooler-bitbar.tar.gz"
+    url "https://github.com/johnmccabe/vmpooler-bitbar/releases/download/v2.1.0/vmpooler-bitbar.tar.gz"
     
-    sha256 "9b28d9682dea3e70922784cd67eedfc7fede54dc8d92ce5000b5de224d8c3524"
-    version "v2.0.0"
+    sha256 "208674485b2d927ee3a14162fe7f496975d5bed59083bc208630b79785d0b1a8"
+    version "v2.1.0"
   
     bottle :unneeded
   
